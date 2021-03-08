35c1eb0e86a7027f079d12264710b4593d39d997
racha01 <70879490+racha01@users.noreply.github.com>
2021-03-08 22:29:14 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,51 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
-namespace SequentialSearch
+namespace BinarySearch
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[5] { 5, 18, 3, 7, 6 };
-            FindMin(numbers);
-            FindMix(numbers);
-            /*Console.Write("Enter a number to search for :");
-            int searchNumber = Convert.ToInt32(Console.ReadLine());
-           
-            bool found = SequentialSearch(numbers, searchNumber);
-            if (found)
-                Console.WriteLine(searchNumber + " is in array.");
+            Random random = new Random();
+            ArrayList mynums = new ArrayList(9);
+            for (int i = 0; i < 10; i++)
+                mynums.Add(random.Next(10));
+            mynums.Sort();
+
+            int position = -1;
+            while (position == -1)
+            {
+                try
+                {
+                    Console.Write("Enter a number to search for:");
+                    int searchNumber = int.Parse(Console.ReadLine());
+
+                    position = BinarySearch(mynums, searchNumber, 0, mynums.Count - 1);
+                    if (position > -1)
+                        Console.WriteLine("found item index = " + position);
+                    else
+                        Console.WriteLine("Not in the array");
+                }catch (Exception) { }
+            }
+            Console.Write("\nArray items :");
+            for (int i = 0; i <= 9; i++)
+                Console.Write(mynums[i] + " ");
+            Console.ReadLine();
+        }
+        static int BinarySearch(ArrayList arr, int value, int lower, int upper)
+        {
+            if (lower > upper)
+                return -1;
             else
-                Console.WriteLine(searchNumber + " is not in array.");*/
-        }
-        static bool SequentialSearch(int[] arr, int sValue)
-        {
-            for (int index = 0; index < arr.Length; index++)
-                if (arr[index] == sValue)
-                    return true;
-                return false;
-        }
-        static void FindMin(int[] numbers)
-        {
-            int min = numbers[0];
-            for (int i = 0; i < 5; i++)
             {
-                if (numbers[i] <= min)
-                    min = numbers[i];
+                int mid;
+                mid = (int)(upper + lower) / 2;
+                if (value < (int)arr[mid])
+                    return BinarySearch(arr, value, lower, mid - 1);
+                else if (value == (int)arr[mid])
+                    return mid;
+                else
+                    return BinarySearch(arr, value, mid + 1, upper);
             }
-            Console.WriteLine(min);
-        }
-        static void FindMix(int[] numbers)
-        {
-            int mix = numbers[0];
-            for (int i = 0; i < 5; i++)
-            {
-                if (numbers[i] >= mix)
-                    mix = numbers[i];
-            }
-            Console.WriteLine(mix);
         }
     }
 }
